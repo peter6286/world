940738a0dcdde226ddd6595f7137ac22de0247df
peter <z18819828123@gmail.com>
2019-07-25 10:11:52 +0800

    part1

--- newworld.py
+++ newworld.py
@@ -1,13 +1,15 @@
 print("london is the world ")
+#Asia
 print("china")
 print("japan")
 print("korea")
 print("thailand")
 print("saudi")
 print("iran")
-
+#europe
 print("uk")
 print("germany")
 print("france")
 print("Russia")
 print("swiss")
+
